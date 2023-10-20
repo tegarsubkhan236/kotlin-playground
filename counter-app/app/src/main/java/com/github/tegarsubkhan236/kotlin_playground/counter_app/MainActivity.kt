0395f143e7dde0c98b0014334c7efd078698bdb9
tegar <tegarsubkhan236@gmail.com>
2023-10-20 16:02:18 +0700

    counter-app : done

--- counter-app/app/src/main/java/com/github/tegarsubkhan236/kotlin_playground/counter_app/MainActivity.kt
+++ counter-app/app/src/main/java/com/github/tegarsubkhan236/kotlin_playground/counter_app/MainActivity.kt
@@ -3,41 +3,44 @@ package com.github.tegarsubkhan236.kotlin_playground.counter_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.github.tegarsubkhan236.kotlin_playground.counter_app.ui.theme.CounterappTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var count by remember {
+                mutableStateOf(0)
+            }
             CounterappTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Text(
+                        text = count.toString(),
+                        fontSize = 30.sp
+                    )
+                    Button(onClick = {
+                        count++
+                    }) {
+                        Text(text = "Click me! ${count}")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CounterappTheme {
-        Greeting("Android")
     }
 }
